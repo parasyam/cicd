36b60a17dc0b8773a36e44f1985ea8ca971724b0
parasyam <parasyamsundarchowdary@gamail.com>
2025-10-23 16:07:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000; // you can pick any free port
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
